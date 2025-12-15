9b9461f2670a6c098f930f14dc6622370be9518b
Daniel Parkes <dparkes@redhat.com>
2025-12-15 21:14:48 +0100

    Added posix backend changes

---
+++ bin/restart-posix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+run_zgw_posix () {
+  podman run --name zgw-posix \
+           --user 167:167 \
+           -p 7480:7480 \
+           -v zgw-posix-driver:/var/lib/ceph/rgw_posix_driver \
+           -v zgw-posix-db:/var/lib/ceph/rgw_posix_db \
+           -e COMPONENT=zgw-posix \
+           -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-zippy} \
+           -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-zippy} \
+           -e RGW_POSIX_BASE_PATH=/var/lib/ceph/rgw_posix_driver \
+           -e RGW_POSIX_DATABASE_ROOT=/var/lib/ceph/rgw_posix_db \
+           -dt quay.io/mmgaggle/zgw-posix:latest
+}
+
+if [[ $(podman ps -a -f "status=running,name=zgw-posix" --format="{{.ID}}") ]] ; then
+  podman kill zgw-posix
+  podman rm zgw-posix
+  run_zgw_posix
+else
+  echo "zgw-posix container is not running, starting"
+  run_zgw_posix
+fi
